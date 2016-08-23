663715d22621c26db99b26500acfb4d9f4354471
Matt Allan <matthew.james.allan@gmail.com>
2016-08-23 12:59:33 -0400

    make phpcs happy

--- src/Util.php
+++ src/Util.php
@@ -21,7 +21,11 @@ class Util
                     return $k . ':' . JsonGuard\as_string($v);
                 }, array_keys($error['constraints']), $error['constraints'])
             );
-            $error['value'] = is_object($error['value']) || is_array($error['value']) ? json_encode($error['value']) : JsonGuard\as_string($error['value']);
+            if (is_array($error['value']) || is_object($error['value'])) {
+                $error['value'] = json_encode($error['value']);
+            } else {
+                $error['value'] = JsonGuard\as_string($error['value']);
+            }
             return array_values($error);
         }, $errors);
     }
